a3545b2993052060edc3c46151d9d1f23e3f1de0
Kenji Okimoto <kenji.okimoto@repro.io>
2020-05-05 11:37:12 +0900

    Improve s3_compressor_parquet.rb

--- examples/fluent-plugin-s3/s3_compressor_parquet.rb
+++ examples/fluent-plugin-s3/s3_compressor_parquet.rb
@@ -1,44 +1,78 @@
 module Fluent::Plugin
   class S3Output
     class ParquetCompressor < Compressor
-      S3Output.register_compressor('parquet', self)
+      S3Output.register_compressor("parquet", self)
 
       config_section :compress, multi: false do
-        config_param :schema_type, :string
+        desc "parquet compression codec"
+        config_param :parquet_compression_codec, :enum, list: [:uncompressed, :snappy, :gzip, :lzo, :brotli, :lz4, :zstd], default: :snappy
+        desc "parquet file page size"
+        config_param :parquet_page_size, :size, default: 8192
+        desc "parquet file row group size"
+        config_param :parquet_row_group_size, :size, default: 128 * 1024 * 1024
+        desc "record data format type"
+        config_param :record_type, :enum, list: [:avro, :csv, :jsonl, :msgpack, :tsv, :json], default: :msgpack
+        desc "schema type"
+        config_param :schema_type, :enum, list: [:avro, :bigquery], default: :avro
+        desc "path to schema file"
         config_param :schema_file, :string
-        config_param :record_type, :string
       end
 
       def configure(conf)
         super
-        check_command('columnify', '-h')
-      end	
+        check_command("columnify", "-h")
+
+        if @compress.parquet_compression_codec.include?(:lzo, :brotli, :lz4)
+          raise Fluent::ConfiError, "unsupported compression codec: #{@compress.parquet_compression_codec}"
+        end
+
+        @parquet_compression_codec = @compress.parquet_compression_codec.to_s.upcase
+        if @compress.record_type == :json
+          @record_type = :jsonl
+        else
+          @record_type = @compress.record_type
+        end
+      end
 
       def ext
-        'parquet'.freeze
+        "parquet".freeze
       end
 
       def content_type
-        'application/octet-stream'.freeze
+        "application/octet-stream".freeze
       end
 
       def compress(chunk, tmp)
-        if @buffer_type == 'file'
-          columnify(@compress.schema_type, @compress.schema_file, @compress.record_type, chunk.path, tmp.path)
-        else
-          Tempfile.create("chunk-parquet-tmp") { |w|
-            w.binmode
-            chunk.write_to(w)
-            w.close
-            columnify(@compress.schema_type, @compress.schema_file, @compress.record_type, w.path, tmp.path)
-          }
+        chunk_is_file = @buffer_type == "file"
+        path = if chunk_is_file
+                 chunk.path
+               else
+                 w = Temfile.new("chunk-parquet-tmp")
+                 w.binmode
+                 chunk.write_to(w)
+                 w.close
+                 w.path
+               end
+        res = columnify(path, tmp.path)
+        raise "failed to execute columnify command. status = #{$?}" unless res
+      ensure
+        unless chunk_is_file
+          w.close(true) rescue nil
         end
       end
 
       private
 
-      def columnify(schema_type, schema_file, record_type, src_path, dst_path)
-        system "columnify -schemaType #{schema_type} -schemaFile #{schema_file} -recordType #{record_type} #{src_path} > #{dst_path}"
+      def columnify(src_path, dst_path)
+        system("columnify",
+               "-parquetCompressionCodec", @parquet_compression_codec,
+               "-parquetPageSize", @compress.parquet_page_size.to_s,
+               "-parquetRowGroupSize", @compress.parquet_row_group_size.to_s,
+               "-recordType", @record_type.to_s,
+               "-schemaType", @compress.schema_type.to_s,
+               "-schemaFile", @compress.schema_file,
+               "-output", dst_path,
+               src_path)
       end
     end
   end
